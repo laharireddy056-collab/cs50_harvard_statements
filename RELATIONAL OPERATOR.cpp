52ea0691964dd273290ac48d45fd52ec983f3af8
Lahari Reddy <laharireddy056@gmail.com>
2025-11-24 22:04:19 +0530

    Relational Operators

---
+++ RELATIONAL OPERATOR.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a=10,b=2;
+	printf("a>b:%d",a>b);
+	printf("\na<b:%d",a<b);
+	printf("\na<=b:%d",a<=b);
+	printf("\na==b:%d",a==b);
+	printf("\na!=b:%d",a!=b);
+}
