20d3dbada98fd4fb0f35652880f66d7f64b9306d
Lahari Reddy <laharireddy056@gmail.com>
2026-04-30 09:54:42 +0530

    String Formatting

---
+++ String Formatting.py
@@ -0,0 +1,7 @@
+  name="lahari"
+age=18
+print(f"My name is {name} and I'm {age} years old") #Using f' string
+print("My name is {} and I'm {} years old".format(name,age)) #Using format
+print("My name is {0} and I'm {1} years old".format(name,age)) #Using format
+print("My name is {n} and I'm {a} years old".format(n=name,a=age)) #Using format 
+print("My name is %s and I'm %d years old" %(name,age)) #Using format specifier
