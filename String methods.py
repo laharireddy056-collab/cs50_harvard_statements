7c5fc28e99f12f7dc18b5918fb62a194fe2ee747
Lahari Reddy <laharireddy056@gmail.com>
2026-04-30 09:51:20 +0530

    String methods

---
+++ String methods.py
@@ -0,0 +1,17 @@
+a="PYthOn pRoGrAMMing"
+print(a.lower())
+pint(a.upper())
+print(a.title())
+print(a.capitalize())
+print(a.isdigit())
+print(a.isalpha())
+print(a.isalnum())
+print(a.index('o'))
+print(a.count('p'))
+print(a.split())
+print(a.strip())
+print(a.lstrip())
+print(a.rstrip())
+print(a.replace("PY","Java"))
+
+
