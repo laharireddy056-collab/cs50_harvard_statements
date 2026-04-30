90baebd926df84f6b80aa7b5fe00e68bbbdd2c80
Lahari Reddy <laharireddy056@gmail.com>
2026-04-30 09:35:31 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,96 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a=6
+b=4
+print("sum is : ",a+b)
+sum is :  10
+print("subtraction is:",a-b)
+subtraction is: 2
+print("Multiplication is:",a*b)
+Multiplication is: 24
+print("Division is:",a/b)
+Division is: 1.5
+print("Modulus is:",a%b)
+Modulus is: 2
+print("Floor Divison is:",a//b)
+Floor Divison is: 1
+print("Exponent is:",a**b)
+Exponent is: 1296
+print(a<b)
+False
+print(a>b)
+True
+print(a<=b)
+False
+print(a>=b)
+True
+print(a==b)
+False
+print(a!=b)
+True
+a=5
+b=7
+a==b
+False
+a+=b
+print(a)
+12
+a-=b
+print(a)
+5
+
+
+a=True
+b=False
+print(a and b)
+False
+print(a or b)
+True
+print(not a)
+False
+
+
+a=3
+b=9
+>>> 
+>>> print(a&b)
+1
+>>> print(a|b)
+11
+>>> print(a^b)
+10
+>>> print(~a)
+-4
+>>> print(a>>b)
+0
+>>> print(a<<b)
+1536
+>>> 
+>>> 
+>>> a=5
+>>> b=8
+>>> c=5
+>>> print(a is c)
+True
+>>> print(b is c)
+False
+>>> print(a is b)
+False
+>>> print(a is not b)
+True
+>>> print(a is not c)
+False
+>>> 
+>>> 
+>>> a=[1,2,3,4]
+>>> b=[2,3,4,5]
+>>> print(1 in a)
+True
+>>> print(5 in a)
+False
+>>> print(12 in a)
+False
+>>> print(2 not in b)
+False
+>>> print(5 not in a)
+True
