1c0c8cca2b568db8012b219d19f32284ba8c602f
Lahari Reddy <laharireddy056@gmail.com>
2026-04-30 09:36:05 +0530

    Type conversions

---
+++ Type Conversions.py
@@ -0,0 +1,20 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=7
+>>> b=3.9
+>>> c=a+b
+>>> print(c)
+10.9
+>>> print(type(a))
+<class 'int'>
+>>> print(type(b))
+<class 'float'>
+>>> a='12'
+>>> print(type(a))
+<class 'str'>
+>>> a=int(a)
+>>> sum=a*a
+>>> print(sum)
+144
+>>> print(type(a))
+<class 'int'>
