d3988e09dbef7968e9c63e99129d98dbddaaa9a1
Lahari Reddy <laharireddy056@gmail.com>
2026-04-30 09:50:11 +0530

    Simple Comprehensions

---
+++ Simple Comprehensions.py
@@ -0,0 +1,18 @@
+#List Comprehension
+given_list=[X for X in range(5)]
+print(given_list)
+
+new_list=[var+3 for var in given_list]
+print(new_list)
+
+
+#Dictionary Comprehension
+given_list={X for X in range
+
+
+
+
+#Set Comprehension
+
+
+#Generator Comprehension
