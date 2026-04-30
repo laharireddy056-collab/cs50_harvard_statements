8d574f1c7e6df56ad90e13f40a4cfc0916c3619e
Lahari Reddy <laharireddy056@gmail.com>
2026-04-30 09:31:18 +0530

    Compound Interest

---
+++ Compound Interest.py
@@ -0,0 +1,7 @@
+p=float(input("Enter principal amount-"))
+r=float(input("Enter rate of interest-"))
+t=float(input("Enter time in years-"))
+amount=p*(1+r/100)**t
+CI=amount-p
+print("Total Amount=",amount)
+print("Compound Interest=",CI)
