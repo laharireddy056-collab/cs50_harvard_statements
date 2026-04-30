4058fd3865489d050c2c53abafa19f8c74a2079b
Lahari Reddy <laharireddy056@gmail.com>
2026-04-30 09:34:29 +0530

    Palindrome or not

---
+++ Palindrome or not.py
@@ -0,0 +1,12 @@
+n=int(input("Enter a number-"))
+temp=n
+rev=0
+while n>0:
+    digit=n%10
+    rev=rev*10+digit
+    n=n//10
+if rev==temp:
+    print("Paindrome")
+else:
+    print("Not a Palindrome")
+    
