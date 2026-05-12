41da05068df7672e2495b95caedb36ad4783e988
Lahari Reddy <laharireddy056@gmail.com>
2026-05-12 10:50:19 +0530

    Double linked list insertions

---
+++ Double linked list insertions.c
@@ -0,0 +1,180 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+
+struct Node *head = NULL;
+
+// Insert at beginning
+void insertBeginning(int value) {
+    struct Node *newNode;
+
+    newNode = (struct Node*)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->prev = NULL;
+    newNode->next = head;
+
+    if (head != NULL) {
+        head->prev = newNode;
+    }
+
+    head = newNode;
+
+    printf("%d inserted at beginning\n", value);
+}
+
+// Insert at end
+void insertEnd(int value) {
+    struct Node *newNode, *temp;
+
+    newNode = (struct Node*)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (head == NULL) {
+        newNode->prev = NULL;
+        head = newNode;
+    }
+    else {
+        temp = head;
+
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+
+        temp->next = newNode;
+        newNode->prev = temp;
+    }
+
+    printf("%d inserted at end\n", value);
+}
+
+// Insert at specific position
+void insertPosition(int value, int pos) {
+    struct Node *newNode, *temp;
+    int i;
+
+    newNode = (struct Node*)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+
+    // Insert at first position
+    if (pos == 1) {
+        newNode->prev = NULL;
+        newNode->next = head;
+
+        if (head != NULL) {
+            head->prev = newNode;
+        }
+
+        head = newNode;
+
+        printf("%d inserted at position %d\n", value, pos);
+        return;
+    }
+
+    temp = head;
+
+    for (i = 1; i < pos - 1 && temp != NULL; i++) {
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        printf("Invalid Position\n");
+        return;
+    }
+
+    newNode->next = temp->next;
+    newNode->prev = temp;
+
+    if (temp->next != NULL) {
+        temp->next->prev = newNode;
+    }
+
+    temp->next = newNode;
+
+    printf("%d inserted at position %d\n", value, pos);
+}
+
+// Display list
+void display() {
+    struct Node *temp;
+
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    temp = head;
+
+    printf("Doubly Linked List: ");
+
+    while (temp != NULL) {
+        printf("%d <-> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+}
+
+int main() {
+    int choice, value, pos;
+
+    while (1) {
+
+        printf("\n--- Doubly Linked List Insertions ---\n");
+        printf("1. Insert at Beginning\n");
+        printf("2. Insert at End\n");
+        printf("3. Insert at Specific Position\n");
+        printf("4. Display\n");
+        printf("5. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+
+            case 1:
+                printf("Enter value: ");
+                scanf("%d", &value);
+
+                insertBeginning(value);
+                break;
+
+            case 2:
+                printf("Enter value: ");
+                scanf("%d", &value);
+
+                insertEnd(value);
+                break;
+
+            case 3:
+                printf("Enter value: ");
+                scanf("%d", &value);
+
+                printf("Enter position: ");
+                scanf("%d", &pos);
+
+                insertPosition(value, pos);
+                break;
+
+            case 4:
+                display();
+                break;
+
+            case 5:
+                exit(0);
+
+            default:
+                printf("Invalid Choice\n");
+        }
+    }
+
+    return 0;
+}
